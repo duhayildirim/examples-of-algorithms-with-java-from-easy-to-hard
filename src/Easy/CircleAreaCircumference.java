718364f447ba19adda8e81dfa9445674d2629762
Duha YILDIRIM <1duhayildirim@gmail.com>
2022-11-29 07:01:56 +0300

    circle area circumference

---
+++ src/Easy/CircleAreaCircumference.java
@@ -0,0 +1,20 @@
+package Easy;
+
+import java.util.Scanner;
+
+public class CircleAreaCircumference {
+    public static void main(String[] args) {
+        double area, circumference, PI = 3.14;
+        int r;
+
+        System.out.print("Enter to radius: ");
+        Scanner input = new Scanner(System.in);
+        r = input.nextInt();
+
+        area = PI * Math.pow(r, 2);
+        circumference = 2 * PI * r;
+
+        System.out.println("circumference: " + circumference);
+        System.out.print("area: " + area);
+    }
+}
