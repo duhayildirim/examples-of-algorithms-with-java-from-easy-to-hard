8e153388b636c87602fd67cab391f3c95f382929
Duha YILDIRIM <1duhayildirim@gmail.com>
2022-11-30 06:55:48 +0300

    add even and multiple of 4

---
+++ src/Middle/SumEvenAndMultipleOfFour.java
@@ -0,0 +1,19 @@
+package Middle;
+
+import java.util.Scanner;
+
+public class SumEvenAndMultipleOfFour {
+    public static void main(String[] args) {
+        int sum = 0, target = 0, i = 0;
+        Scanner input = new Scanner(System.in);
+
+        while (target % 2 == 0) {
+            System.out.println("Enter to number");
+            target = input.nextInt();
+            if (target % 2 == 0 && target % 4 == 0) {
+                sum += target;
+            }
+        }
+        System.out.println(sum);
+    }
+}
