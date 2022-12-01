ad3d5cc18eed281ad4b34981c35ab7a7f02e7166
Duha YILDIRIM <1duhayildirim@gmail.com>
2022-12-01 15:18:42 +0300

    recursive pow

---
+++ src/Hard/RecursivePow.java
@@ -0,0 +1,23 @@
+package Hard;
+
+import java.util.Scanner;
+
+public class RecursivePow {
+    static int pow(int n1, int n2) {
+        int result;
+        result = n2 == 0 ? 1 : n1 * pow(n1, n2 - 1);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int n1, n2;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("n1 : ");
+        n1 = input.nextInt();
+        System.out.print("n2 : ");
+        n2 = input.nextInt();
+
+        System.out.println(pow(n1, n2));
+    }
+}
